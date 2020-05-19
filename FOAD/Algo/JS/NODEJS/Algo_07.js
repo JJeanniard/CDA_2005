6d32a3c92324874108757be84c5198752276d346
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-05-19 09:51:24 +0200

    Attente du module Maths sur isard

--- FOAD/Algo/JS/NODEJS/Algo_07.js
+++ FOAD/Algo/JS/NODEJS/Algo_07.js
@@ -1,18 +1,18 @@
 //echec de l'exo méthode non comprise
 let biblioOfName = ["agathe", "berthe", "chloé", "cunégonde", "olga", "raymonde", "sidonie"]
 
-let valeurSaissir = "raymonde";
+let valeurSaissi = "raymonde";
 
 function rec(tab, val, a, b){
     //b taille de mon tableau
     console.log(val);
 
     if(a >= b){
-        if(valeurSaissir.localeCompare(tab[a]) == 0){
+        if(val.localeCompare(tab[a]) == 0){
             return a;
         }
 
-        if(a > 0 && valeurSaissir.localeCompare(tab[a-1]) == 0){
+        if(a > 0 && val.localeCompare(tab[a-1]) == 0){
             return a - 1;
         }
         return -1;
@@ -20,9 +20,10 @@ function rec(tab, val, a, b){
 
     var m = Math.floor((a+b)/2);
     console.log(m);
-    if(valeurSaissir.localeCompare(tab[m]) <= 0){
+    if(val.localeCompare(tab[m]) <= 0){
         return rec(tab, val, a, m);
     }else{
+        console.log(tab + " " + val + " " + (m+1) + " " + b);
         return rec(tab, val, m+1, b)
     }
 }
@@ -31,7 +32,7 @@ function search(tab, val){
     return rec(tab, val, 0, tab.length-1);
 }
 
-let i = search(biblioOfName, valeurSaissir);
+let i = search(biblioOfName, valeurSaissi);
 
 console.log("index " + i);
 
