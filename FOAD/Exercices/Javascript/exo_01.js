171f117aa6845ed2224417abe468f087592fd101
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-05-19 21:22:57 +0200

    premiere partie de l'exo

--- FOAD/Exercices/Javascript/exo_01.js
+++ FOAD/Exercices/Javascript/exo_01.js
@@ -37,9 +37,38 @@
 /**
  * @class Employee
  */
-class Employee
-{
-    // A vous de jouer...
+class Employee {
+    constructor(_id, _lastname, _firstname, _role, _salaryBt, _hiredate) {
+        this.id = _id;
+        this.lastname = _lastname;
+        this.firstname = _firstname;
+        this.email = (_firstname[0] + _lastname).toLowerCase() + "@arfp.asso.fr";
+        this.role = _role;
+        this.salaryBt = _salaryBt;
+        this.hiredate = _hiredate.getFullYear() + "-" + (_hiredate.getMonth() + 1) + "-" + _hiredate.getDate();
+        this.hiredateObj = _hiredate; 
+    }
+
+    getMonthlySalary = function () {
+        // (salaire_mensuel = salaire_annuel / 12 * 0.75)
+        return Math.round(this.salaryBt / 12 * 0.75);
+    }
+
+    getSeniority = function () {
+        this.dateDay = new Date();
+
+        this.yearOfBirt = this.dateDay.getFullYear() - this.hiredateObj.getFullYear();
+        this.monthOfBirt = this.dateDay.getMonth() + 1 - this.hiredateObj.getMonth();
+        this.dayOfBirt = this.dateDay.getDate() - this.hiredateObj.getDate();
+
+        if (this.yearOfBirt >= 2) {
+            return this.yearOfBirt + " ans " + this.monthOfBirt + " mois " + this.dayOfBirt + " jours";
+        } else if (this.yearOfBirt < 2 && this.monthOfBirt >= 3) {
+            return this.monthOfBirt + " mois " + this.dayOfBirt + " jours";
+        } else {
+            return this.dayOfBirt + " jours";
+        }
+    }
 }
 
 
@@ -49,9 +78,12 @@ class Employee
 
 /** @var Employee employee1 */
 var employee1 = new Employee(1, 'Doe', 'John', 'manager', 82000, new Date('2020-12-28')); // création d'un nouvel employé
-
+var employee2 = new Employee(2, 'Amza', 'Red', 'ouvrier', 21000, new Date('2016-03-02'));
+var employee3 = new Employee(3, 'tarat', 'Blue', 'sous-fifre', 18000, new Date('2016-03-02'));
+var employee4 = new Employee(4, 'Turit', 'Yellow', 'boss', 150000, new Date('1990-05-31'));
+var employee5 = new Employee(5, 'Lomol', 'Brown', 'stagiaire', 9000, new Date('1998-03-02'));
 /** @var array employees */
-const employees = [employee1]; // tableau contenant les employés
+const employees = [employee1, employee2, employee3, employee4, employee5]; // tableau contenant les employés
 
 console.log(employee1); // doit afficher l'objet "employee1"
 console.log("Il y a " + employees.length + " employé(e)s."); // doit afficher "Il y a 5 employé(e)s."
@@ -64,3 +96,4 @@ console.log(employees); // export des employés dans la console
 
 // Écrivez votre code à partir de la ligne suivante...
 
+console.log(employee5.getSeniority());
