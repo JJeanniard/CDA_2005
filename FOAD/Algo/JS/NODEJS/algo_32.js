3f26dce477cc54f838dc640f185d3490b24457e0
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-05-25 10:19:06 +0200

    correction du Tp

--- FOAD/Algo/JS/NODEJS/algo_32.js
+++ FOAD/Algo/JS/NODEJS/algo_32.js
@@ -6,7 +6,20 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-if("." == mot[mot.length-1]){
+if (mot.length === 1) {
+    console.log("Pas de mot!");
+    return false;
+}
+
+if(mot === '.'){
+    console.log("Ceci est pas un mot!")
+    return false;
+}
+
+if ("." !== mot[mot.length - 1]) {
+    console.log("Le mot finit pas part un point!");
+    return false;
+}
 
 rl.question("Entrer la lettre, pour retouner le nombre d'occurence: ", answer => {
 
@@ -15,24 +28,17 @@ rl.question("Entrer la lettre, pour retouner le nombre d'occurence: ", answer =>
     //TODO verifier si c'est bien un caractere
 
     while (i < mot.length) {
-        if (answer == mot[i]) {
-            nbrOcurence = nbrOcurence + 1;
-        } else if (answer == "." && i == mot.length) {
-            return console.error("La phrase est vide!");
+        if (answer === mot[i]) {
+            nbrOcurence++;
         }
         i++;
     }
 
     if (nbrOcurence >= 1) {
-        return console.info("La lettre est présente plusieurs fois!");
+        return console.info("La lettre est présente ",nbrOcurence," fois!");
     } else {
         return console.info("La lettre n'est pas présente!");
     }
 
 });
 
-}else if(mot[mot.length-1] != -1){
-    console.log("Pas de mot!")
-} else {
-    console.log("Le mot finit pas part un point!")
-}
