1f41ec6d36eec002d3a918e9c339c2923d1eab2d
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-05-25 09:50:00 +0200

    oublie de la condition du point

--- FOAD/Algo/JS/NODEJS/algo_32.js
+++ FOAD/Algo/JS/NODEJS/algo_32.js
@@ -1,29 +1,38 @@
 const readline = require('readline');
-var mot = "anticonstitutionnellement";
+var mot = "anticonstitutionnellement.";
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+if("." == mot[mot.length-1]){
+
 rl.question("Entrer la lettre, pour retouner le nombre d'occurence: ", answer => {
+
     let i = 0, nbrOcurence = 0;
 
     //TODO verifier si c'est bien un caractere
-
 
     while (i < mot.length) {
         if (answer == mot[i]) {
             nbrOcurence = nbrOcurence + 1;
         } else if (answer == "." && i == mot.length) {
-            console.error("La phrase est vide!")
+            return console.error("La phrase est vide!");
         }
         i++;
     }
 
     if (nbrOcurence >= 1) {
-        return console.info("La lettre est présente plusieurs fois!")
+        return console.info("La lettre est présente plusieurs fois!");
     } else {
-        return console.info("La lettre n'est pas présente!")
+        return console.info("La lettre n'est pas présente!");
     }
+
 });
+
+}else if(mot[mot.length-1] != -1){
+    console.log("Pas de mot!")
+} else {
+    console.log("Le mot finit pas part un point!")
+}
