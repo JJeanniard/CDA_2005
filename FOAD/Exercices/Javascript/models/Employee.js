3a63e717b3f18e3f35af50424492f6a56369945b
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-05-28 13:02:16 +0200

    corretion & reorganisation + creation models

--- FOAD/Exercices/Javascript/models/Employee.js
+++ FOAD/Exercices/Javascript/models/Employee.js
@@ -30,8 +30,6 @@
  * Tutoriel Javascript: https://www.pierre-giraud.com/javascript-apprendre-coder-cours/introduction/ 
  */
 
-//TODO: QTM Commenter les consoles du patron 
-
 /* COMPLÉTEZ LE PROGRAMME... */
 
 /**
@@ -39,89 +37,96 @@
  */
 class Employee {
 
-    constructor(_id, _lastname, _firstname, _role, _salaryBt, _hiredate) {
-        this.id = _id;
+    constructor(_id, _lastname, _firstname, _role, _salary, _hiredate) {
+        this.id = parseInt(_id || 0);
         this.lastname = _lastname;
         this.firstname = _firstname;
         this.email = this.firstname[0].concat(this.lastname).toLowerCase().concat("@email.fr");
         this.role = _role;
-        this.salaryBt = _salaryBt;
-        this.hiredate = _hiredate;//TODO: il manque le 0 quand il y a qu'une unité
+        this.salary = _salary;
+        this.hiredate = _hiredate;
     }
 
     getMonthlySalary() {
         // (salaire_mensuel = salaire_annuel / 12 * 0.75)
-        return Math.round(this.salaryBt / 12 * 0.75);
+        return Math.round(this.salary / 12 * 0.75);
     }
 
     getSeniority() {
-        //TODO: le systeme de date me convenais pas
-        this.dateToDay = new Date();
+        //TODO: Corrigée le calcule de date dégative
+        const dateToDay = new Date();
+        let date;
+        
+        let diff = dateToDay.getTime() - this.hiredate.getTime();
 
-        let diff = this.dateToDay.getTime() - this.hiredate.getTime();
+        let years = dateToDay.getFullYear() - this.hiredate.getFullYear();
+        let month = dateToDay.getMonth() - this.hiredate.getMonth();
+        let day = dateToDay.getDate() - this.hiredate.getDate();
 
-        let yearsD = this.dateToDay.getFullYear() - this.hiredate.getFullYear();
-
-        let monthD = ((this.dateToDay.getMonth() + 12 * this.dateToDay.getFullYear()) - (this.hiredate.getMonth() + 12 * this.hiredate.getFullYear())) - (yearsD * 12);
-
-        let dayD = Math.round((diff / (24 * 3600 * 1000)) - (yearsD * 365.25 + monthD * 30));
-
-        if (yearsD >= 2) {
-            return yearsD + " ans " + monthD + " mois " + (dayD > -1 ? dayD : 0) + " jours";
-        } else if (yearsD < 2 && monthD >= 3) {
-            return monthD + " mois " + dayD + " jours";
-        } else {
-            return dayD + " jours";
+        if(month <= 0){
+            years--;
+            month += 12;
         }
 
-    }
+        if(day <= 0){
+            month--;
+            day = Math.floor(day + 365.25/12);
+        }
 
-    timesForSenior = function () {
-        return this.diff;
+        if (years >= 2) {
+            date =  years + " ans " + month + " mois " + day + " jours";
+        } else if (years < 2 && month >= 3) {
+            date = month + " mois " + day + " jours";
+        } else {
+            date = day + " jours";
+        }
+        return date;
     }
 }
+
+module.exports = Employee;
 
 /** DÉBUT ZONE NON EDITABLE (Ne pas modifier les lignes suivantes) */
 
 
-/** @var Employee employee1 */
-var employee1 = new Employee(1, 'Doe', 'John', 'manager', 82000, new Date('2020-05-01')); // création d'un nouvel employé
-var employee2 = new Employee(2, 'Amza', 'Red', 'ouvrier', 21000, new Date('2016-03-02'));
-var employee3 = new Employee(3, 'tarat', 'Blue', 'sous-fifre', 18000, new Date('2020-03-02'));
-var employee4 = new Employee(4, 'Turit', 'Yellow', 'boss', 150000, new Date('1990-05-30'));
-var employee5 = new Employee(5, 'Lomol', 'Brown', 'stagiaire', 9000, new Date('1520-05-27'));
-/** @var array employees */
-const employees = [employee1, employee2, employee3, employee4, employee5]; // tableau contenant les employés
+// /** @var Employee employee1 */
+// var employee1 = new Employee(1, 'Doe', 'John', 'manager', 82000, new Date('2020-05-01')); // création d'un nouvel employé
+// var employee2 = new Employee(2, 'Amza', 'Red', 'ouvrier', 21000, new Date('2016-03-02'));
+// var employee3 = new Employee(3, 'tarat', 'Blue', 'sous-fifre', 18000, new Date('2020-03-02'));
+// var employee4 = new Employee(4, 'Turit', 'Yellow', 'boss', 150000, new Date('1990-05-30'));
+// var employee5 = new Employee(5, 'Lomol', 'Brown', 'stagiaire', 9000, new Date('1520-05-27'));
+// /** @var array employees */
+// const employees = [employee1, employee2, employee3, employee4, employee5]; // tableau contenant les employés
 
-console.log(employee1); // doit afficher l'objet "employee1"
-console.log("Il y a " + employees.length + " employé(e)s."); // doit afficher "Il y a 5 employé(e)s."
-console.log(employees); // export des employés dans la console
-
-
-/** FIN ZONE NON EDITABLE (Ne pas modifier les lignes précédentes) */
+// console.log(employee1); // doit afficher l'objet "employee1"
+// console.log("Il y a " + employees.length + " employé(e)s."); // doit afficher "Il y a 5 employé(e)s."
+// console.log(employees); // export des employés dans la console
 
 
-
-// Écrivez votre code à partir de la ligne suivante...
+// /** FIN ZONE NON EDITABLE (Ne pas modifier les lignes précédentes) */
 
 
 
-let f = function(a, b){
-    return a.hiredate - b.hiredate;
-}
+// // Écrivez votre code à partir de la ligne suivante...
 
-employees.sort(f);
 
-for (let index = 0; index < employees.length; index++) {
-    const element = employees[index];
-    console.log("########################")
-    console.log("nom: " + element.lastname);
-    console.log("Prénom: " + element.firstname);
-    console.log("Email: " + element.email);
-    console.log("Role: " + element.role);
-    console.log("Salaire Net: " + element.getMonthlySalary());
-    console.log("Anciennete: " + element.getSeniority() + "");
-}
+
+// let f = function(a, b){
+//     return a.hiredate - b.hiredate;
+// }
+
+// employees.sort(f);
+
+// for (let index = 0; index < employees.length; index++) {
+//     const element = employees[index];
+//     console.log("########################")
+//     console.log("nom: " + element.lastname);
+//     console.log("Prénom: " + element.firstname);
+//     console.log("Email: " + element.email);
+//     console.log("Role: " + element.role);
+//     console.log("Salaire Net: " + element.getMonthlySalary());
+//     console.log("Anciennete: " + element.getSeniority() + "");
+// }
 
 
 
