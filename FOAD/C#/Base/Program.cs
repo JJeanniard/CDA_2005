f414a1f9157dbfebc4eb030c3175378fd9dd2b06
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-06-07 09:20:42 +0200

    Creation de mon premier programme en c# pour apprendre les bases

---
+++ FOAD/C#/Base/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String welcom;
+            String firstname;
+
+            welcom = "Bonjour";
+            firstname = "jonas";
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"{welcom} à {firstname}");
+
+        }
+    }
+}
