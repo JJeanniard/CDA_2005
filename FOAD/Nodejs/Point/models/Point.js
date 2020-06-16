3d07272e626f109a22252fc0d3e131a2705a716a
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-06-16 15:55:16 +0200

    create point.md pour disocier les consignes du fichier de travail

--- FOAD/Nodejs/Point/models/Point.js
+++ FOAD/Nodejs/Point/models/Point.js
@@ -1,22 +1,4 @@
-/**
- * JAVASCRIPT:  Exercices
- * 
- * Définition d'un "Point"
- * 
- * 
- * CONSIGNES : 
- * 1) Lisez la description et étudiez la classe "Point" ci-dessous (celle-ci est partiellement implémentée)
- * 2) Implémentez les méthodes suivantes:
- *  - move(int x, int y)    : définit de nouvelles coordonnées (x=abscisses, y=ordonnées)
- *  - duplicate()           : retourne une nouvelle instance de "Point" avec les mêmes coordonnées que l'instance actuelle
- *  - copy(Point _point)    : copie les coordonnées du "Point" fourni en argument dans l'instance actuelle
- *  - rabbit(Point _point)  : Les coordonnées de l'instance actuelle et du "Point" fourni en argument sont échangées
- * 
- * RESTRICTIONS :
- *  - Le corps de la méthode "rabbit" ne peut contenir que 3 instructions au maximum !
- * 
- * Le petit programme qui suit la classe doit fonctionner en l'état (non modifié)
- */
+
 
 /**
  * La classe "Point" représente les coordonnées d'un point dans une zone à 2 dimensions
