ff35216f78f4f47e018e41e8ccff3839fdc21823
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-07-08 18:32:39 +0200

    correction mineur dans l'uml & init de C# avec debut de test

--- FOAD/C#/POO_C_P/Compte.cs
+++ FOAD/C#/POO_C_P/Compte.cs
@@ -6,6 +6,96 @@ namespace Bank
 {
     class Compte
     {
+        private int id;
+        private string lastname;
+        private int solde;
+        private int decouvert;
 
+        public Compte()
+        {
+        }
+
+        public Compte(int _id, string _lastname, int _solde, int _decouvert)
+        {
+            id = _id;
+            lastname = _lastname;
+            solde = _solde;
+            decouvert = _decouvert;
+        }
+
+        /// <summary>
+        /// Retourne les informations du compte client.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "Id: " + id + "\nLastname: " + lastname + "\nSolde: " + solde + "\nDecouvert autoriser: " + decouvert;
+        }
+        /// <summary>
+        /// Action qui permet de crediter le compte
+        /// </summary>
+        /// <param name="_somme"></param>
+        public void Crediter(int _somme)
+        {
+            solde += _somme;
+        }
+        /// <summary>
+        /// Actino qui permet de debiter sur le compte,
+        /// avec verification de decouvert autoriser
+        /// </summary>
+        /// <param name="_somme"></param>
+        /// <returns></returns>
+        public bool Debiter(int _somme)
+        {
+            bool isAccepter;
+            isAccepter = true;
+
+            if (solde - _somme < decouvert + solde)//300 - 500 (100)
+            {
+                isAccepter = false;
+            }
+            else
+            {
+                solde = solde - _somme;
+            }
+
+            return isAccepter;
+        }
+        /// <summary>
+        /// Action de virement d'une somme à sur un autre compte 
+        /// </summary>
+        /// <param name="_somme"></param>
+        /// <param name="_compteD"></param>
+        /// <returns></returns>
+        public bool Virement(int _somme, Compte _compteD)
+        {
+            bool isAccepter;
+            isAccepter = Debiter(_somme);
+
+            _compteD.Crediter(_somme);
+
+            return isAccepter;
+        }
+
+        /// <summary>
+        /// Action qui compare la somme des deux comptes
+        /// </summary>
+        /// <param name="_compteD"></param>
+        /// <returns></returns>
+        public bool Compare(Compte _compteD)
+        {
+            bool isSuperieur;
+
+            if (solde > _compteD.solde)
+            {
+                isSuperieur = true;
+            }
+            else
+            {
+                isSuperieur = false;
+            }
+
+            return isSuperieur;
+        }
     }
 }
--- FOAD/C#/POO_C_P/Program.cs
+++ FOAD/C#/POO_C_P/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
+
 namespace Bank
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Compte compte1 = new Compte(1, "Doe", 3000, -100);
+            Compte compte2 = new Compte(2, "Jhg", 200, 0);
+            Compte compte3 = new Compte(3, "fdsf", 565, -200);
+
+            Console.WriteLine("Bienvenue Dans la Bank XXX");
+
+            Console.WriteLine(compte1.ToString() + "\n");
+            Console.WriteLine(compte2.ToString() + "\n");
+            Console.WriteLine(compte3.ToString() + "\n");
+
+            compte2.Crediter(50);
+            Console.WriteLine(compte2.ToString() + "\n");
+
+            Console.WriteLine(compte3.Debiter(500));
+            Console.WriteLine(compte3.ToString() + "\n");
+
         }
     }
 }
