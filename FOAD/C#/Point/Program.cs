0eb553cbbbf0ae9f838702e65294bf11e8b386a1
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-07-12 10:31:32 +0200

    petit méthode pour mettre à jour une collection d'objet

---
+++ FOAD/C#/Point/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point> coords = new List<Point>();
+
+            Point coord1 = new Point();
+            Point coord2 = new Point();
+            Point coord3 = new Point();
+            Point coord4 = new Point(-11, 30);
+            Point coord5 = new Point(10, 5);
+            Point coord6 = new Point(coord3);
+            
+            coords.Add(coord1);
+            coords.Add(coord2);
+            coords.Add(coord3);
+            coords.Add(coord4);
+            coords.Add(coord5);
+            coords.Add(coord6);
+
+            Console.WriteLine("Hello World, welcome in classe point!");
+
+            int i;
+            i = 1;
+            foreach (Point _point in coords)
+            {
+                Console.WriteLine("Point: "+i+" coordonnée: " + _point.ToString());
+                i++;
+            }
+
+            coord5.DeplacePoint(60, 60);
+            UpdateInCollecPoint(coords, coord5);
+
+            i = 1;
+            foreach (Point _point in coords)
+            {
+                Console.WriteLine("Point: " + i + " coordonnée: " + _point.ToString());
+                i++;
+            }
+
+        }
+
+        public static bool UpdateInCollecPoint(List<Point> _collection ,Point _points)
+        {
+            bool isValide;
+            int value;
+            isValide = true;
+
+            value = _collection.IndexOf(_points);
+
+            _collection.Remove(_points);
+
+            _collection.Insert(value, _points);
+
+            return isValide;
+        }
+    }
+}
