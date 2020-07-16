61a44834e38cc111fb5a4e48c3238511a68faea8
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-07-16 16:27:40 +0200

    debut de la fin de l'implementation des dernieres méthode

--- FOAD/C#/POO_C_P/Bank.cs
+++ FOAD/C#/POO_C_P/Bank.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GstBancaire
 {
-    class Bank
+    public class Bank
     {
         private int nbComptes;
         private List<Compte> comptes = new List<Compte>();
@@ -18,19 +19,47 @@ namespace GstBancaire
             name = _name;
         }
 
-        public string ToString()
+        public override string ToString()
         {
-            return "";
+            int i;
+            string answer;
+            answer = "";
+
+            for (i = 0; i < comptes.Count; i++)
+            {
+                answer += comptes[i].ToString() + "\n";
+            }
+
+            return answer;
         }
 
-        public void AddCompte(int num, string lastname, int solde, int decouvertAutoriser)
+        public void AddCompte(int _num, string _lastname, int _solde, int _decouvertAutoriser)
         {
-
+            AddCompte(new Compte(_num, _lastname, _solde, _decouvertAutoriser));
         }
 
         private void AddCompte(Compte _compte)
         {
+            comptes.Add(_compte);
+        }
+
+        public void HideCompte()
+        {
+            //regarder le sytéme de trie sur List
+        }
+
+        public Compte showCompte(int _num)
+        {
 
         }
+
+        public bool Switch(int _num1, int _num2, int somme)
+        {
+            bool isValid;
+            isValid = false;
+
+            return isValid;
+        }
+
     }
 }
