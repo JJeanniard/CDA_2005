1063e86b52e47abc293e5b0ece56f65caf65f6d6
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-07-16 08:01:30 +0200

    debut d'implementation et passage au cours FR

--- FOAD/C#/POO_C_P/Bank.cs
+++ FOAD/C#/POO_C_P/Bank.cs
@@ -8,5 +8,29 @@ namespace GstBancaire
     {
         private int nbComptes;
         private List<Compte> comptes = new List<Compte>();
+
+        private string name;
+        private string ville;
+
+        public Bank(string _name, string _ville)
+        {
+            ville = _ville;
+            name = _name;
+        }
+
+        public string ToString()
+        {
+            return "";
+        }
+
+        public void AddCompte(int num, string lastname, int solde, int decouvertAutoriser)
+        {
+
+        }
+
+        private void AddCompte(Compte _compte)
+        {
+
+        }
     }
 }
