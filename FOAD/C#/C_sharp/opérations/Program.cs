326bb6ccd796990233944bab58353bce07e74902
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-07-22 20:52:19 +0200

    Operation simple

---
+++ FOAD/C#/C_sharp/opérations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace opérations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
