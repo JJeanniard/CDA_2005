e3fbd3f3e1e732b94bcbec7aad51d6fcbcffb876
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-07-22 19:38:18 +0200

    valeur litéralle

--- FOAD/C#/C_sharp/My_first_app/Program.cs
+++ FOAD/C#/C_sharp/My_first_app/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace My_first_app
+namespace Valeur_litterales
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine('b');//valeur littérales
+            Console.WriteLine("Hello World!");//erreur si on passe en litérrales
+            Console.WriteLine(123);//int
+            Console.WriteLine(12.3m);//littéral décimal avec le suffixe m or M
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+
+            //String valeur different avec une valeur littéral
+            Console.WriteLine("123");
+            Console.WriteLine(123);
+
+            Console.WriteLine("true");
+            Console.WriteLine(true);
         }
     }
 }
