79bc86e0fba956522aaeb2ce62654439bbe1bc5b
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-10-07 08:39:50 +0200

    Lepidoptera

---
+++ FOAD/C#/Mini_Tp/Lepidoptera/Lepidoptera.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lepidoptera
+{
+    public class Lepidoptera
+    {
+
+        private string etat;
+
+        private int nbrPattes;
+
+        private float vitesseDeplacement;
+
+        public Lepidoptera() : this("Chenille", 8, 2)
+        {
+        }
+
+        public Lepidoptera(string _etat, int _nbrPattes, float _vitesseDeplacement)
+        {
+            etat = _etat;
+            nbrPattes = _nbrPattes;
+            vitesseDeplacement = _vitesseDeplacement;
+        }
+
+        public void VtsDeplacement(float _vitesse)
+        {
+            vitesseDeplacement = _vitesse;
+        }
+
+        public void Metamorphose(string _etat)
+        {
+            etat = _etat;
+        }
+    }
+}
