4c23c943c879e0f245795328ef608911bb09d12e
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-10-27 09:53:01 +0100

    init de la classe impot

---
+++ FOAD/Php/Calcul_impots/Impot.php
@@ -0,0 +1,39 @@
+<?php
+
+class Impot
+{
+
+    private const taux14 = 14;
+
+    private const taux9 = 9;
+
+    private string $name;
+
+    private int $revenu;
+
+    private float $resultat;
+
+    public function __construct($name, $revenu)
+    {
+        $this->name = $name;
+        $this->revenu = $revenu;
+    }
+
+    public function afficheImpot(): void
+    {
+        echo "$this->name votre impôt est de $this->resultat euros.";
+    }
+
+    public function calculImpot(): float
+    {
+
+        if ($this->revenu > 15000) {
+            $this->resultat = $this->revenu / self::taux14 * 100;
+        } else {
+            $this->resultat = $this->revenu / self::taux9 * 100;
+        }
+
+        return $this->resultat;
+    }
+
+}
