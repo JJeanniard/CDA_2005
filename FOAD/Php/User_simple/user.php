58813abdda13dd8c8c33d09b59bb2820ba50deba
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-10-27 11:56:24 +0100

    fin du l'exo user

--- FOAD/Php/User_simple/user.php
+++ FOAD/Php/User_simple/user.php
@@ -13,10 +13,84 @@ $dsn = 'mysql:host=localhost;port=3306;dbname=db_users_simple;charset=utf8';
 
 $dbconnect = new PDO($dsn, "root", "");
 
-$row = $dbconnect->prepare("SELECT * FROM users_view WHERE user_id = :id");
+/**
+ * CONSIGNES :
+ *
+ * Si le paramètre 'id' est fourni dans l'url:
+ *      Afficher l'utilisateur correspondant à l'id fourni
+ *      Afficher 'aucun résultat' si aucun utilisateur trouvé
+ * Sinon
+ *      Afficher tous les utilisateurs
+ *
+ */
 
 $id = $_GET['id'] ?? 0;
 $id = intval($id);
+
+if($id > 0){
+
+    $row = $dbconnect->prepare("SELECT * FROM users_view WHERE user_id = :id");
+
+    $row->execute(array(':id' => $id));
+
+    $user = $row->fetch();
+    if($user){
+        echo $user['user_name'];
+    }else{
+        echo "aucun résultat";
+    }
+
+
+}else{
+
+    $row = $dbconnect->query("SELECT * FROM users_view");
+    foreach ($row->fetchall() as $user){
+        echo '<pre>';
+        echo $user['user_name'];
+        echo '</pre>';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$id = intval($id);
 
 if ($id > 0) {
 
@@ -28,7 +102,7 @@ if ($id > 0) {
     }
 } else {
     echo "Il faut obligatoirement un id";
-}
+}*/
 ?>
 
 </body>
