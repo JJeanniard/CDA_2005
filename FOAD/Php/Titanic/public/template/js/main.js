5a026ee625857717bc43c6c2d671b1a18702ba4a
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-11-23 08:35:22 +0100

    include de js

--- FOAD/Php/Titanic/public/template/js/main.js
+++ FOAD/Php/Titanic/public/template/js/main.js
@@ -1,17 +1,28 @@
-(function() {
+function menu() {
+    //todo : revoir ceci
     let actmenu = document.getElementById("act-menu");
-    let sousMenu = document.getElementById("sous-menu");
+
+    let sousMenu = actmenu.getElementsByTagName("ul");
 
 
     function openMenu(){
-        if(sousMenu.hidden){
-            sousMenu.removeAttribute("hidden");
-            sousMenu.setAttribute("id", "sous-menu");
-        }else {
-            sousMenu.setAttribute("hidden", true);
+        if (!sousMenu.hidden) {
+            console.log("test1");
             sousMenu.removeAttribute("id", "sous-menu");
+            sousMenu.setAttribute("id", "sous-menu1");
+            sousMenu.setAttribute("hidden", "true");
+
+        } else {
+            console.log("test2");
+            sousMenu.setAttribute("hidden", "false");
+            sousMenu.removeAttribute("id", "sous-menu");
+            sousMenu.setAttribute("id", "sous-menu1");
+
         }
     }
 
     actmenu.addEventListener("click", openMenu);
-})();
+
+
+
+}
