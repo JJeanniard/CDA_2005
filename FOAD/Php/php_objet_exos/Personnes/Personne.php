727ef99bb3e69166dab98e4e90c5e9d8a67d811c
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-11-23 17:19:10 +0100

    reglage date d'anniversaire

--- FOAD/Php/php_objet_exos/Personnes/Personne.php
+++ FOAD/Php/php_objet_exos/Personnes/Personne.php
@@ -3,5 +3,51 @@
 
 class Personne
 {
+    private string $name;
+
+    private string $prenom;
+
+    private int $age;
+
+    public function __construct(string $name, string $prenom, DateTime $datedenaissance)
+    {
+        $toDay = new DateTime();
+
+        $diff = $toDay->diff($datedenaissance);
+
+        $this->age = $diff->y;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAge(): int
+    {
+        return $this->age;
+    }
 
 }
--- FOAD/Php/php_objet_exos/TestPersonnes/index.php
+++ FOAD/Php/php_objet_exos/TestPersonnes/index.php
@@ -1,1 +1,6 @@
 <?php
+require "../Personnes/Personne.php";
+
+$personne = new Personne("tata", "toto", new DateTime("1990-03-14"));
+
+echo $personne->getAge();
