f41a05cd5ccb0f882026565ee88a268614ded3eb
Jonasbadboys <jonathanjeanniard@sfr.fr>
2021-01-07 17:57:13 +0100

    debut de la fin du loyout

--- TP_Freelancer/Tp_Freelance/Controllers/HomeController.cs
+++ TP_Freelancer/Tp_Freelance/Controllers/HomeController.cs
@@ -1,22 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Tp_Freelance.Models;
 
 namespace Tp_Freelance.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
